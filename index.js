842fc6827885cad263dd515d23bf6ffb8c9e1863
weilps <guillaume.gaudin75@gmail.com>
2023-03-04 13:15:13 +1100

    js 0.2

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const addCircle = (id, size) => {
-  id = document.createElement("span");
-  id.classList.add("circle");
-  id.style.height = size + "px";
-  id.style.width = size + "px";
-  id.addEventListener("mousemove", (e) => {
-    console.log(e);
-  });
+const addCircle = (name, iD, size) => {
+  name = document.createElement("span");
+  name.classList.add("circle");
+  document.body.appendChild(name);
+  //   name.setAttribute("id", iD);
+  name.id = iD;
 };
 
-addCircle("c1", 300);
+addCircle("c1", "c1");
+addCircle("c2", "c2");
+addCircle("c3", "c3");
+
+document.addEventListener("mousemove", (e) => {
+  c1.style.top = e.pageY + "px";
+  c1.style.left = e.pageX + "px";
+  c2.style.top = e.pageY + "px";
+  c2.style.left = e.pageX + "px";
+  c3.style.top = e.pageY + "px";
+  c3.style.left = e.pageX + "px";
+});
